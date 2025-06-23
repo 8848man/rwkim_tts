4329794b889e91ba46a3522ca55ab013aef4039e
8848man <83057037+8848man@users.noreply.github.com>
2025-06-23 17:54:41 +0900

    initial commit

--- lib/features/tts_service/widgets/character_select_box.dart
+++ lib/features/tts_service/widgets/character_select_box.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rwkim_tts/features/tts_service/enums/voice_characters.dart';
+import 'package:rwkim_tts/features/tts_service/providers/now_character_provider.dart';
 
 class CharacterSelectBox extends ConsumerWidget {
-  const CharacterSelectBox({super.key});
+  CharacterSelectBox({super.key});
+
+  final List<VoiceCharacters> voiceCharacters = [
+    VoiceCharacters.dustin,
+    VoiceCharacters.walt,
+    VoiceCharacters.geomac,
+    VoiceCharacters.yejin,
+    VoiceCharacters.mira,
+    VoiceCharacters.neo,
+    VoiceCharacters.allen,
+  ];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -12,11 +24,11 @@ class CharacterSelectBox extends ConsumerWidget {
         spacing: 16, // 좌우 간격
         runSpacing: 16, // 위아래 간격
         children: [
-          buildCharacterSelectBox('철수', 'assets/images/character1.png'),
-          buildCharacterSelectBox('영희', 'assets/images/character2.png'),
-          buildCharacterSelectBox('민수', 'assets/images/character3.png'),
-          buildCharacterSelectBox('지영', 'assets/images/character4.png'),
-          buildCharacterSelectBox('수빈', 'assets/images/character5.png'),
+          ...List.generate(
+            voiceCharacters.length,
+            (index) =>
+                buildCharacterSelectBox(context, ref, voiceCharacters[index]),
+          ),
           // 더 추가 가능
         ],
       ),
@@ -24,24 +36,40 @@ class CharacterSelectBox extends ConsumerWidget {
   }
 }
 
-Widget buildCharacterSelectBox(String name, String imagePath) {
-  return SizedBox(
-    width: 80,
-    height: 104,
-    child: Column(
-      children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundImage: AssetImage(
-            'assets/images/character.png',
-          ), // Replace with your character image
+Widget buildCharacterSelectBox(
+  BuildContext context,
+  WidgetRef ref,
+  VoiceCharacters character,
+) {
+  return GestureDetector(
+    onTap: () {
+      ref.read(nowCharacterProvider.notifier).update((state) => character);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('${character.name} 캐릭터가 선택되었습니다.'),
+          duration: const Duration(milliseconds: 700),
         ),
-        const SizedBox(height: 8),
-        Text(
-          name,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-      ],
+      );
+      Navigator.of(context).pop(); // 다이얼로그 닫기
+    },
+    child: SizedBox(
+      width: 80,
+      height: 104,
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: AssetImage(
+              character.imagePath, // 캐릭터 이미지 경로
+            ), // Replace with your character image
+          ),
+          const SizedBox(height: 8),
+          Text(
+            character.name, // 캐릭터 이름
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     ),
   );
 }
